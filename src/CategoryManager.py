350a12724f2583f9e944d131c2d3dc6d0db79edf
Allan He-Chen <allanhechen@gmail.com>
2025-02-28 22:08:22 -0500

    Add workbook writing functionality

--- src/CategoryManager.py
+++ src/CategoryManager.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from datetime import date, timedelta
-from openpyxl import Workbook
+from openpyxl import Workbook, worksheet
+from openpyxl.worksheet.worksheet import Worksheet
 from AbstractCategory import Category
 from Expense import Expense
 
@@ -11,10 +12,12 @@ class CategoryManager:
         categories: set[Category],
         blocked_category: Category,
         catch_all_category: Category,
+        concerned_date: date,
     ):
         self._categories = categories
         self._blocked_category = blocked_category
         self._catch_all_category = catch_all_category
+        self._date_ranges = CategoryManager.find_date_ranges(concerned_date)
 
     def test_expense(self, expense: Expense):
         if self._blocked_category.test_expense(expense):
@@ -26,23 +29,59 @@ class CategoryManager:
 
         self._catch_all_category.test_expense(expense)
 
-    def fill_workbook(self, workbook: Workbook, concerned_date: date):
+    def fill_workbook(self, workbook: Workbook):
         all_categories = list(self._categories)
         all_categories.append(self._catch_all_category)
         all_categories.append(self._blocked_category)
 
         for category in all_categories:
-            CategoryManager._fill_category(workbook, category, concerned_date)
+            self._fill_category(workbook, category)
 
-    @staticmethod
-    def _fill_category(workbook: Workbook, category: Category, concerned_date: date):
-        workbook.create_sheet(category._name)
-        date_ranges = CategoryManager.find_date_ranges(concerned_date)
+    def _fill_category(self, workbook: Workbook, category: Category):
+        worksheet: Worksheet = workbook.create_sheet(category.get_name())
+        expense_groups = category.get_expense_groups()
 
-        for [starting_date, ending_date] in date_ranges:
-            month_expenses = category.get_expenses(starting_date, ending_date)
-            # TODO: actually derive a way to write categories into their own sheets
-            print(month_expenses)
+        worksheet["A2"] = "Supplier"
+        worksheet["B2"] = "Description"
+
+        for col_offset, date_range in enumerate(self._date_ranges, 2):
+            col = CategoryManager.get_col_index(col_offset)
+            [_, ending_date] = date_range
+            worksheet[col + "2"] = "Amount"
+            worksheet[col + "1"] = f"Week of {ending_date.strftime('%b %d')}"
+
+        for offset, expense_group in enumerate(expense_groups, 2):
+            row = CategoryManager.get_row_index(offset)
+
+            worksheet["A" + row] = expense_group.get_supplier_name()
+            worksheet["B" + row] = expense_group.get_description()
+
+            for col_offset, [starting_date, ending_date] in enumerate(
+                self._date_ranges, 2
+            ):
+                col = CategoryManager.get_col_index(col_offset)
+                relevant_expenses = expense_group.get_expenses(
+                    starting_date, ending_date
+                )
+                total_expenses = 0
+                for expense in relevant_expenses:
+                    total_expenses += expense.debit_amount_cents
+
+                worksheet[col + row] = total_expenses / 100
+
+        # C column + length + 1 blank
+        final_col = 1 + len(self._date_ranges)
+        final_col_index = CategoryManager.get_col_index(final_col)
+        total_col_index = CategoryManager.get_col_index(final_col + 2)
+
+        worksheet[total_col_index + "1"] = "Total"
+        worksheet[total_col_index + "2"] = "Amount"
+
+        for row_offset in range(len(expense_groups)):
+            row = CategoryManager.get_row_index(row_offset + 2)
+            worksheet[total_col_index + row] = f"=SUM(C{row}:{final_col_index+ row})"
+
+        CategoryManager.adjust_col_size(worksheet)
 
     @staticmethod
     def find_date_ranges(concerned_date: date) -> list[list[date]]:
@@ -78,3 +117,25 @@ class CategoryManager:
         result.append([range_start_date, current_day])
 
         return result
+
+    @staticmethod
+    def get_col_index(offset: int):
+        return chr(ord("A") + offset)
+
+    @staticmethod
+    def get_row_index(offset: int):
+        return chr(ord("1") + offset)
+
+    @staticmethod
+    def adjust_col_size(worksheet: Worksheet):
+        for col in worksheet.columns:
+            max_length = 0
+            col_letter = col[0].column_letter  # type: ignore
+            for cell in col:
+                try:
+                    if cell.value:
+                        max_length = max(max_length, len(str(cell.value)))
+                except:
+                    pass
+
+            worksheet.column_dimensions[col_letter].width = max_length + 2
--- src/main.py
+++ src/main.py
@@ -12,17 +12,24 @@ from InputWorkbook import InputWorkbook
 
 if __name__ == "__main__":
     filename = input("Input file name: ")
+    month_index = input("Input month index: ")
     expenses = InputWorkbook.read_workbook(filename)
+    month = date(date.today().year, int(month_index), 1)
 
     categories: set[Category] = set([MarketingCategory(), MealEntertainmentCategory()])
     blocked_category = BlockedCategory()
     catch_all_category = CatchAllCategory()
 
-    category_manager = CategoryManager(categories, blocked_category, catch_all_category)
+    category_manager = CategoryManager(
+        categories, blocked_category, catch_all_category, month
+    )
 
     for expense in expenses:
         category_manager.test_expense(expense)
 
     output_workbook = Workbook()
-    february = date(date.today().year, 2, 1)
-    category_manager.fill_workbook(output_workbook, february)
+    category_manager.fill_workbook(output_workbook)
+
+    if "Sheet" in output_workbook.sheetnames:
+        output_workbook.remove(output_workbook["Sheet"])
+    output_workbook.save(f"Expenses month of {month.strftime("%B")}.xlsx")
